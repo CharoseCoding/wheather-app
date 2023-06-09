dc6f5a540af8764db26e7d11bdeb59a8bdffaa61
CharoseCoding <130360001+CharoseCoding@users.noreply.github.com>
2023-06-09 02:32:02 +0100

    current location fix

--- week6.js
+++ week6.js
@@ -126,6 +126,12 @@ function getCurrentLocation(event) {
   navigator.geolocation.getCurrentPosition(searchLocation);
 }
 
+function searchLocation(position) {
+  let apiKey = "fda3688b1db05987dd5d07c237aecfba";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
 function convertToFahrenheit(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
